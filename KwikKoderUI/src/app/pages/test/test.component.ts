45df212bef558be76059941cb9da58d58fb2f40f
Juniper Song <songminseonk@gmail.com>
2021-06-29 11:34:50 -0700

    commit before changing lanes

--- KwikKoderUI/src/app/pages/test/test.component.ts
+++ KwikKoderUI/src/app/pages/test/test.component.ts
@@ -85,10 +85,7 @@ export class TestComponent implements OnInit {
     //get content to type
     this.api.getTestContentByCatagoryId(id).then(
       (obj)=> {
-        // Rainbow.color();
         this.testmat = obj;
-        //this.testmat.content= obj.content;
-        //this.testmat.author = obj.author;
         this.state.words = this.testmat.content;
         this.state.wordarray = this.state.words.split('');
         this.state.wordarray= this.state.wordarray.filter(this.isBadChar);
