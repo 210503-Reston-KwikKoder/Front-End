51c014959d52ac1032e014d34520cc21861ccfc7
Phoebe <jia.520@outlook.com>
2021-06-30 10:35:47 -0700

    added tests for player-test-area

--- KwikKoderUI/src/app/components/player-test-area/player-test-area.component.spec.ts
+++ KwikKoderUI/src/app/components/player-test-area/player-test-area.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DisplayCategoryPipe } from '../../pipes/display-category.pipe';
 
 import { PlayerTestAreaComponent } from './player-test-area.component';
 
@@ -6,20 +8,33 @@ describe('PlayerTestAreaComponent', () => {
   let component: PlayerTestAreaComponent;
   let fixture: ComponentFixture<PlayerTestAreaComponent>;
 
+  @Pipe({name: 'displayCategory'})
+    class MockPipe implements PipeTransform{
+      transform(value: number): number{
+        return value;
+      }
+    }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlayerTestAreaComponent ]
+      declarations: [ PlayerTestAreaComponent, MockPipe],
+      providers: [
+        {provide: DisplayCategoryPipe, useClass: MockPipe},
+      ]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
+  it('should create', () => {
     fixture = TestBed.createComponent(PlayerTestAreaComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ngOnInit should call', () => {
+    fixture = TestBed.createComponent(PlayerTestAreaComponent);
+    component = fixture.componentInstance;
+    expect(component.ngOnInit()).toHaveBeenCalled;
+  });
+
 });
