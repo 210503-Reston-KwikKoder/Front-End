0cd3c4171884cdecb34e9cf344c9929f76df5b46
Phoebe <jia.520@outlook.com>
2021-06-30 10:36:26 -0700

    added initial test for que.component

--- KwikKoderUI/src/app/components/que/que.component.spec.ts
+++ KwikKoderUI/src/app/components/que/que.component.spec.ts
@@ -1,16 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { QueComponent } from './que.component';
+import { QueService } from 'src/Services/que.service';
+import { HttpClient } from '@angular/common/http';
+import { Socket } from 'ngx-socket-io';
 
 describe('QueComponent', () => {
   let component: QueComponent;
   let fixture: ComponentFixture<QueComponent>;
+  let queue: QueService;
+  let http: HttpClient;
+  let socket : Socket;
+
+  class MockQueService {}
+  class MockHttp {}
+  class MockSocket {}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QueComponent ]
+      declarations: [ QueComponent ],
+      providers: [
+        {provide: QueService, useClass: MockQueService},
+        {provide: HttpClient, useClass: MockHttp},
+        {provide: Socket, useClass: MockSocket},
+      ]
     })
     .compileComponents();
+    queue = TestBed.inject(QueService);
+    http = TestBed.inject(HttpClient);
+    socket = TestBed.inject(Socket);
+
   });
 
   beforeEach(() => {
