db09eb5f87d43200a93a36c5edefbd8b5ed88591
Phoebe <jia.520@outlook.com>
2021-06-30 10:39:51 -0700

    added initial tests for - chat.service - comp-function.service - que.service.service

--- KwikKoderUI/src/Services/comp-functions.service.spec.ts
+++ KwikKoderUI/src/Services/comp-functions.service.spec.ts
@@ -1,16 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { flush, getTestBed, TestBed } from '@angular/core/testing';
+import { CompetitionTestResults } from 'src/Models/CompetitionTestResults';
 
 import { CompFunctionsService } from './comp-functions.service';
+import { RestService } from './rest.service';
 
 describe('CompFunctionsService', () => {
   let service: CompFunctionsService;
+  let rest: RestService;
+
+  class MockRestService
+  {
+    getCompetitionContent(id: number):Promise<any>{
+      return new Promise<any>((resolve, reject) => {})
+    };
+    postCompetitionResults(model : CompetitionTestResults){};
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+
+      providers: [
+        {provide: RestService, useClass: MockRestService}
+      ]
+    });
+
     service = TestBed.inject(CompFunctionsService);
+    rest = TestBed.inject(RestService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call', () => {
+
+    expect(service.newTest()).toHaveBeenCalled;
+
+  });
+
 });
